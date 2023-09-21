6bd397bae6c834ca478633d6ab776291faa25c5c
AshkanArabim <ashkan.arabim@gmail.com>
2023-09-21 03:56:23 -0600

    add Makefile

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -9,9 +9,9 @@ int non_space_char(char c) {
   return c != '\0' || !space_char(c);
 }
 
-char *token_start(char *str){
-  // TODO...
-}
+/* char *token_start(char *str){ */
+/*   // TODO... */
+/* } */
 
 /* char **tokenize(char* str) { */
 /* } */
