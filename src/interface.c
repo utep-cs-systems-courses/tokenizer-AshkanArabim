7bf2b1dd799d07d68bb5a1e2b2284cdba5bb98f7
AshkanArabim <ashkan.arabim@gmail.com>
2023-10-02 01:58:36 -0600

    cleanup

--- src/interface.c
+++ src/interface.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include "tokenizer.h"
-#include "history.h"
 
-int get_stdin_id(char line[100]){
-  line++; // skip the '^' at the beginning
+#include "history.h"
+#include "tokenizer.h"
+
+int get_stdin_id(char line[100]) {
+  line++;  // skip the '^' at the beginning
   // -1 means user entered ^ with no following digits.
   int id = -1;
   for (int i = 0; line[i] >= 48 && line[i] < 58; i++) {
@@ -19,11 +20,15 @@ int get_stdin_id(char line[100]){
 int main() {
   printf("Welcome to Ashkan's tokenizer!\n");
   printf("Enter any sentence to see it tokenized.\n");
-  printf("To quit, enter 'q' (anything starting with 'q' ends the program. If you have a sentence like that, add a space at the beginning.\n");
-  printf("To access history, use this format: '^x' where [x] represents a number\n");
+  printf(
+      "To quit, enter 'q' (anything starting with 'q' ends the program. If you "
+      "have a sentence like that, add a space at the beginning.\n");
+  printf(
+      "To access history, use this format: '^x' where [x] represents a "
+      "number\n");
 
   List* history = init_history();
-  while(1) {
+  while (1) {
     printf("quit: 'q', history: '^[x]'\n");
     printf(">");
     // don't go to next iteration until you reach a newline character.
@@ -32,37 +37,42 @@ int main() {
     /* while(c = getchar()){ */
     /*   printf("%c", c); */
     /* } */
-    
+
     char line[100];
     fgets(line, sizeof(line), stdin);
-    //printf(line);
+    // printf(line);
 
     if (line[0] == 'q') {
       printf("Quit...\n");
       free_history(history);
       break;
+
     } else if (line[0] == '^') {
       int hist_id = get_stdin_id(line);
-      printf("%d\n", hist_id); // DEBUG
+      printf("%d\n", hist_id);  // DEBUG
       if (hist_id == -1) {
-	printf("Your '^' was not followed by digits...\n");
-	continue;
+        printf("Your '^' was not followed by digits...\n");
+        continue;
       }
-      printf("this runsss...\n"); // DEBUG
-      printf("%p\n", history); // DEBUG: works
+      printf("this runsss...\n");  // DEBUG
+      printf("%p\n", history);     // DEBUG: works
       char* hist_line = get_history(history, hist_id);
-      printf("this runs...\n"); // DEBUG
-      printf("History item %i\n: %c\n", hist_id, hist_line);      
+      printf("this runs...\n");  // DEBUG
+      printf("History item %i\n: %s\n", hist_id, hist_line);
+
     } else {
       // add to history
       add_history(history, line);
       // tokenize & print
       char** tokens = tokenize(line);
+      printf("full line: %s", line);  // DEBUG
       print_tokens(tokens);
       // free tokens right now since we
       // won't use them again...
       free_tokens(tokens);
-    }    
+      printf("after being freed vv\n"); // DEBUG: works
+      print_tokens(tokens);  // DEBUG
+    }
   }
 }
 
--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,61 +1,71 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "tokenizer.h"
 
-int space_char(char c) {
-  return c != '\0' && (c == '\t' || c == ' ');
-}
+#include <stdio.h>
+#include <stdlib.h>
 
-int non_space_char(char c) {
-  return c != '\0' && !space_char(c);
-}
+int space_char(char c) { return c != '\0' && (c == '\t' || c == ' '); }
 
-char *token_start(char *str){
+int non_space_char(char c) { return c != '\0' && !space_char(c); }
+
+char *token_start(char *str) {
   // skip characters until you reach a space
   char *sc = str;
-  while (non_space_char(*sc++));
-  return sc; // return pointer to character after space pointer
+  while (non_space_char(*sc))
+    ;
+  return sc;  // return pointer to character after space
 }
 
 char *token_terminator(char *token) {
-  // QUESTION: I don't get the description in the header...
-  char *sc = token;
-  // assuming we're on a word, return the (space) character immediately after the word.
-  while (non_space_char(*sc++));
-  return sc;
+  // assuming we're on a word, return the (space) character immediately after
+  // the word.
+  while (non_space_char(*token++)) {
+    printf("checked %c\n", *token);  // DEBUG: STOPS HERE!!
+  };
+  printf("token terminator ends...\n");  // DEBUG
+  return token;
 }
 
+// int count_tokens(char *str) {
+//   int count = 0;
+//   // iterate over string
+//   while (*str++ != '\0') {
+//     if (non_space_char(*str)) {  // if non space (word) detected
+//       count++;
+//       while (non_space_char(*str++))
+//         ;  // skip all chars in this word
+//     }
+//   }
+//   return count;
+// }
+
+// V2
 int count_tokens(char *str) {
   int count = 0;
-  // iterate over string
-  while(*str++ != '\0') {
-    if (non_space_char(*str)) { // if non space (word) detected
+  while (*str++ != '\0') {
+    printf("counter iteration...");  // DEBUG
+    if (non_space_char(*str)) {
       count++;
-      while(non_space_char(*str++)); // skip all chars in this word
+      // skip str to the space / terminator following this word.
+      str = token_terminator(str);
     }
+    printf("NEW this runs inside the loop\n");
   }
+  printf("NEW this runs\n");
   return count;
 }
 
 char *copy_str(char *inStr, short len) {
   // allocate heap memory (one more than len for \0)
-  char* newstr = malloc(sizeof(char) * (len + 1));
-  char* newstrslider = newstr;
-
-  //// iterate and copy
-  //do{
-    //*newstrslider = *inStr;
-    //newstrslider++;
-  //} while (++inStr);
-
+  char *newstr = malloc(sizeof(char) * (len + 1));
+  char *newstrslider = newstr;
 
   // new and improved copy loop
-  while(non_space_char(*inStr)){
+  while (non_space_char(*inStr)) {
     *newstrslider = *inStr;
     inStr++;
     newstrslider++;
   }
-  
+
   // terminate with zero
   *newstrslider = '\0';
 
@@ -63,20 +73,9 @@ char *copy_str(char *inStr, short len) {
   return newstr;
 }
 
-/* char* skip_letters(char* str) { */
-/*   while(non_space_char(str++)); // skip any non-space character */
-
-/*   // probably not necessary, */
-/*   // but, this will return a null pointer if we reach the end of the string */
-/*   if (*str == '\0') { */
-/*     return NULL; */
-/*   } else { */
-/*     return str; */
-/*   } */
-/* } */
-
-char* skip_spaces(char* str) {
-  while(space_char(*str)); // skip any space characters
+char *skip_spaces(char *str) {
+  while (space_char(*str))
+    ;  // skip any space characters
 
   if (*str == '\0') {
     return NULL;
@@ -85,43 +84,25 @@ char* skip_spaces(char* str) {
   }
 }
 
-char** tokenize(char* str){
-  // V2 vv
+char **tokenize(char *str) {
+  printf("NEW this runs\n");  // DEBUG
   int numtokens = count_tokens(str);
-  char** tokens = malloc(sizeof(char*) * numtokens + 1);
+  printf("count_tokens returned: %d\n", numtokens);  // DEBUG
+  char **tokens = malloc(sizeof(char *) * numtokens + 1);
+  printf("tokenizing each word...\n");  // DEBUG
   for (int i = 0; i < numtokens; i++) {
     int strsize = (token_terminator(str) - str);
+    printf("strsize: %d\n", strsize);
     tokens[i] = copy_str(str, strsize);
     str = token_start(str);
   }
   tokens[numtokens] = NULL;
 
   return tokens;
-  // V2 ^^
-  
-  // int tindex = 0;
-
-  // sc = str // copy of pointer at beginning
-
-    // if is_space(sc):
-    // sc = skip_spaces(sc) --> func?
-
-  //// add the pointers to the start of each token to "tokens"
-  //// option 1: vv don't touch original text, just add tokens --> BAD
-  // while is_not_space(sc):
-    // char* token = sc
-    // tokens[counter] = token;
-    // sc = skip_letters(sc) --> func?
-    // counter++
-
-  //// option 2: vv copy each token to heap, add THAT to "tokens"
-  // while (sc != \0):
-    // if space --> skip
-    // ..
 }
 
 void print_tokens(char **tokens) {
-  for(int i = 0; ; i++) {
+  for (int i = 0;; i++) {
     if (tokens[i] == NULL) {
       return;
     }
@@ -130,14 +111,14 @@ void print_tokens(char **tokens) {
 }
 
 void free_tokens(char **tokens) {
-  for(int i = 0; ; i++) {
-    if (tokens[i] == NULL) { // break at the end of array
+  for (int i = 0;; i++) {
+    if (tokens[i] == NULL) {  // break at the end of array
       return;
     }
     free(tokens[i]);
-    tokens[i] = NULL; // nullify pointer after freeing
+    tokens[i] = NULL;  // nullify pointer after freeing
   }
 
   free(tokens);
-  tokens = NULL; // nullify tokens itself
+  tokens = NULL;  // nullify tokens itself
 }
